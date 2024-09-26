20633a00cc4c7aa537eabbd1220463fb2769e6bc
skillchase <kushagra.garg_bca21@gla.ac.in>
2024-09-26 19:45:23 +0530

    using selector

--- week 7/7.2/week-7-main/src/App.jsx
+++ week 7/7.2/week-7-main/src/App.jsx
@@ -1,19 +1,22 @@
 import { useContext, useMemo, useState } from "react"
-import { CountContext } from "./context";
-import { countAtom } from "./store/atoms/count"
-import { useRecoilValue } from "recoil";
+// import { CountContext } from "./context";
+import { countAtom, evenSelector } from "./store/atoms/count"
+import { RecoilRoot, useRecoilValue, useRecoilState, useSetRecoilState } from "recoil";
 
 
 function App() {
 
   return (
     <div>
+      <RecoilRoot>
         <Count />
+      </RecoilRoot>
     </div>
   )
 }
 
 function Count() {
+  console.log("Count")
   return <div>
     <CountRenderer />
     <Buttons />
@@ -26,19 +29,29 @@ function CountRenderer() {
     <b>
       {count}
     </b>
+    <IsEven></IsEven>
   </div>
 }
 
 function Buttons() {
-  const count =0
+  // const [count, setCount] = useRecoilState(countAtom);
+  const setCount = useSetRecoilState(countAtom);
+  console.log("hello");
   return <div>
     <button onClick={() => {
-      setCount(count + 1)
+      setCount(count => count+1)
     }}>Increase</button>
 
     <button onClick={() => {
-      setCount(count - 1)
+      setCount(count => count-1)
     }}>Decrease</button>
+  </div>
+}
+
+function IsEven(){
+  const count = useRecoilValue(evenSelector);
+  return <div>
+    {count % 2==0 ? "is even " : "is odd"}
   </div>
 }
 
