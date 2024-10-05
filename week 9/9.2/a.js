0687dcc1250005a5e33af55a79fb9cd4c7acb5eb
skillchase <kushagra.garg_bca21@gla.ac.in>
2024-10-05 18:48:44 +0530

    starting with typescript

---
+++ week 9/9.2/a.js
@@ -0,0 +1,5 @@
+"use strict";
+function hello(x) {
+    console.log(x + (Math.random() * 1000));
+}
+hello(12);
---
+++ week 9/9.2/a.ts
@@ -0,0 +1,6 @@
+function hello(x :number){
+    console.log(x+(Math.random()*1000))
+}
+
+
+hello(12)
